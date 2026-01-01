ac8eee1c88d93ebe3aabdca464f2f00524db6a14
Dmitrovskii <146029437+dmitrovskii@users.noreply.github.com>
2026-01-01 14:35:44 +0200

    Додання опису роботи коду

--- main/main.py
+++ main/main.py
@@ -2,12 +2,15 @@ import pyperclip
 
 meds = ["Діаформін","Карведілол","Магнікор", "Соритмік","Мемокс","Клівас","Сибутин","Епадол","Меркана"]
 
+# Вивід з нумерацією списку ліків
 for index, name in enumerate(meds, start=1):
     print(f"{index}. {name}")
 
+# Отримання індексів ліків для подальшого копіювання
 raw_index = input("\nВведіть номери через пробіл:\n").split()
 list_of_need = []
 
+# Перевірка та додавання індексів в list_of_need
 for s in raw_index:
     try:
         index = int(s)
@@ -21,10 +24,12 @@ for s in raw_index:
 
 print("")
 
+# Буфер для копіювання та виводу вибраних ліків
 bufer = []
 for i in list_of_need:
     print(meds[i])
     bufer.append(meds[i])
 
+# Копіювання ліків в буфер ОС
 pyperclip.copy("\n".join(bufer))
 
