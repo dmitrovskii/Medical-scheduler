7cd517859ec009d696d15c321a8dd79e611b3544
Dmitrovskii <146029437+dmitrovskii@users.noreply.github.com>
2026-01-01 14:31:56 +0200

    Створено механізм визначення + копіювання

--- main/main.py
+++ main/main.py
@@ -1,1 +1,30 @@
+import pyperclip
+
+meds = ["Діаформін","Карведілол","Магнікор", "Соритмік","Мемокс","Клівас","Сибутин","Епадол","Меркана"]
+
+for index, name in enumerate(meds, start=1):
+    print(f"{index}. {name}")
+
+raw_index = input("\nВведіть номери через пробіл:\n").split()
+list_of_need = []
+
+for s in raw_index:
+    try:
+        index = int(s)
+
+        if 1 <= index <= len(meds): 
+            list_of_need.append(index - 1)
+        else: print(f"Помилка: Номера {index} немає в списку.")
+
+    except ValueError:
+        print(f"Помилка: '{s}' не є числом!")
+
+print("")
+
+bufer = []
+for i in list_of_need:
+    print(meds[i])
+    bufer.append(meds[i])
+
+pyperclip.copy("\n".join(bufer))
 
