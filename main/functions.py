2634fad67ab0d52ac3f09a9c2d6ad076d187816d
Dmitrovskii <146029437+dmitrovskii@users.noreply.github.com>
2026-01-02 23:43:30 +0200

    Створення нових папок та файлів для полегшеної роботи Написання масивного коду для подальшого розщеплення на модулі

---
+++ main/functions.py
@@ -0,0 +1,57 @@
+import os
+import time
+
+# Функція для очистки консолі
+def clear(ytime):
+    time.sleep(ytime)
+    os.system('cls')
+
+# Допоміжна функція для повідомлення помилки (поки що баговано)
+def ertext(info):
+    print("Невідповідний символ:", info, "!")
+
+# lto - list to output 
+def youinput(lto, text, limit):
+
+    # Зациклює якщо юзер ввів якусь фігню
+    ending = True
+    while ending == True:
+
+        output = []
+        test = input(text).split()
+
+        # змінна для while
+        problem = 0
+
+        for item in test: 
+            
+            # перетворення даних ліста на інт та прохід фільтрів
+            try: 
+
+                index = int(item)
+
+                if 1 <= index <= len(test): # Свідомий баг, зроблений як короткочасна заміна джсон
+                    output.append(index - 1)
+                else: 
+                    print(ertext(item))
+                    problem += 1
+                    continue
+
+            except ValueError: 
+                print(ertext(item))
+        
+        # умова яка вирішує зациклювати чи ні
+        if problem == 0:
+            ending = False
+        else: 
+            ending = True
+            clear(1)
+
+    clear(0)
+    return output, limit
+
+
+# Це лишень тест
+blablacar = youinput(0, "Please enter numbers:\n", 1)
+print(blablacar)
+
---
+++ utils/ui.py
@@ -0,0 +1,8 @@
+import os
+import time
+
+# Функція для очистки консолі
+def clear(ytime):
+    time.sleep(ytime)
+    os.system('cls')
+
