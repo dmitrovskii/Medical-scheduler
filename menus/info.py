15b7c7969daf9f12d277ca7caa994ee6cece63c4
Dmitrovskii <146029437+dmitrovskii@users.noreply.github.com>
2026-01-17 15:06:04 +0200

    Add INFO_MAP and no-op function to info.py
    Implement INFO_MAP to simplify routing within info.py. Add a no-op placeholder function for visual elements makred as "coming soon". This refactors improves code organization and prepare to future feature code updates.

--- menus/info.py
+++ menus/info.py
@@ -1,12 +1,33 @@
 from menus import menu
-from utils import ui
+from utils import ui, actions
+
+FILEPATH = "./config/info_m.json"
 
 def run():
-    choose = menu.loop_menu("./config/info_m.json", header="Інформація")
+    while True:
+        choose = menu.loop_menu(FILEPATH, header="Інформація")
+        action_name = actions.take_action(choose, FILEPATH)
+        back = actions.route(action_name, INFO_MAP)
 
-    if choose == 4:
-        return
+        if back == "return": 
+            break
+
+
     
+def add():
+    print("Coming soon")
+    return True
+
+def delete():
+    print("Coming soon")
+    return False
     
 def show(path="./data"): 
     ui.list_dir(path)
+
+
+INFO_MAP = {
+    "info_show": show,
+    "add": add,
+    "delete": delete
+}
