1a596b73b9cb771b74b2d572dbe9ceeac2b48d96
dmitrovskii <deniskrot99@gmail.com>
2026-02-22 12:20:43 +0200

    refactor: rewrite main() to use MenuHandler class
    Note: ROUTE and info.py not working yet

--- main.py
+++ main.py
@@ -1,19 +1,46 @@
-from menus import menu
-from utils import actions, ui
-from config import MAIN
-import router
+import os
+import time
+
+import config
+from utils import mjson, ui
+from menus import info, settings, about
+from menus.menu import MenuHandler
+
+ROUTES = {
+    "info": info.run,
+    "settings": settings.run,
+    "about": about.run
+}
 
 def main():
+    path_to_file = config.MAIN['menu']
 
-    while True:
-        main_menu = menu.loop_menu(MAIN['menu'])   
-        res = actions.take_action(main_menu, MAIN['menu'])
-        ui.clear()
-        if res == "exit":
-            break
-        
-        
-        router.run(res)
+    mdict = mjson.load(path_to_file)
+    menu = MenuHandler(path_to_file)
+
+    try: 
+        while True:
+            print(" --- MAIN INFO --- \n")
+            ui.index_dict_name(mdict)   
+
+            raw_input = input('\nEnter: ')
+            valid_indices = menu.process_input(raw_input)
+
+            if valid_indices:
+                index = valid_indices[0]
+                action_name = menu.get_action_by_index(index)
+                ui.clear()
+
+                if action_name == "exit":
+                    break
+                menu.route_data(action_name, ROUTES)
+
+            time.sleep(1) 
+            ui.clear(3)
+
+    except KeyboardInterrupt: 
+        print("\nClose program...")
+
 
 if __name__ == '__main__': 
     main()
